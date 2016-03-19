fac6a9b28428f7d9ba0e7ef735e3f87ca569cb51
Ian Tinsley <itinsley@gmail.com>
2016-03-19 22:15:50 +1100

    Make functions asynchronous

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var rates = require('./lib/rates');
 
-rates.index()
+rates.all()
   .then(log)
   .then(function(){
     console.log("not a promise so it stops here..")
   })
-
 
 function log(string){
   return new Promise(function(resolve, reject){
--- test/lib/ratesTest.js
+++ test/lib/ratesTest.js
@@ -4,28 +4,34 @@ var Rates = require("../../lib/rates");
 
 describe("Rates", function() {
 
+  var rates;
+  before(function(done){
+    Rates.all()
+      .then(function(_rates){
+        rates = _rates;
+        done();
+      });
+  })
+
   describe("Indexing rates", function() {
     it("should index all rates by Currency Code", function() {
-      Rates.index().then(function(indexed){
-        expect(Object.keys(indexed).length).to.equal(6);
-      });
+      indexed = Rates.indexed(rates);
+      expect(Object.keys(indexed).length).to.equal(6);
     });
     it("should store an instance of each rate object", function() {
-      Rates.index().then(function(indexed){
-        console.log(indexed);
-        audCad = indexed['AUD-CAD'];
-        expect(audCad['from']).to.equal('AUD');
-        expect(audCad['to']).to.equal('CAD');
-        expect(audCad['conversion']).to.equal('1.0079');
-      });
+      indexed = Rates.indexed(rates);
+      audCad = indexed['AUD-CAD'];
+      expect(audCad['from']).to.equal('AUD');
+      expect(audCad['to']).to.equal('CAD');
+      expect(audCad['conversion']).to.equal('1.0079');
     });
   });
 
   describe("distinctCodes", function(){
     it("should return list of unique currency codes", function() {
-      Rates.distinctCodes().then(function(codes){
-        assert.deepEqual(codes, ['AUD', 'CAD', 'EUR', 'USD']);
-      });
+      codes = Rates.distinctCodes(rates);
+      console.log(codes)
+      assert.deepEqual(codes, ['AUD', 'CAD', 'EUR', 'USD']);
     });
   });
 
