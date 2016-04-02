953f032b8a2c38af9e2e65d410f069e52d08dd64
Ian Tinsley <itinsley@gmail.com>
2016-04-02 22:32:25 +1100

    Filter Transactions by Sku

--- index.js
+++ index.js
@@ -9,19 +9,17 @@ var ratesFile = __dirname + '/puzzle/SAMPLE_RATES.xml';
 var transFile = __dirname + '/puzzle/TRANS.csv';
 var transFile = __dirname + '/puzzle/SAMPLE_TRANS.csv';
 
-var fetchRates = RatesData.all(ratesFile);
+var fetchRatesData = RatesData.all(ratesFile);
 var transStream = Transactions.stream(transFile);
 
 var parse = require('csv-parse');
 
-// // Now that setup is done, write data to the stream
-// parser.write("root:x:0:0:root:/root:/bin/bash\n");
-// parser.write("someone:x:1022:1022:a funny cat:/home/someone:/bin/bash\n");
-// Close the readable stream
+fetchRatesData.then(function(ratesData){
+  rates = Rates.all(ratesData);
+  console.log(rates)
+})
 
-
-
-Transactions.findAllBySku(transStream, 'DM1182').then(function(trans){
+Transactions.filterBySku(transStream, 'DM1182').then(function(trans){
   console.log(trans);
 });
 
