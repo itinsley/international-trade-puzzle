2a114c78a0a298069870c9d512e208449655ab3d
Ian Tinsley <itinsley@gmail.com>
2016-04-02 23:28:38 +1100

    Calculate Total
    - Correct answer for Sample Data
    - Move logic into libs

--- index.js
+++ index.js
@@ -6,21 +6,38 @@ var RatesData = require('./lib/ratesData');
 var Transactions = require('./lib/transactions');
 
 var ratesFile = __dirname + '/puzzle/SAMPLE_RATES.xml';
-var transFile = __dirname + '/puzzle/TRANS.csv';
 var transFile = __dirname + '/puzzle/SAMPLE_TRANS.csv';
+
+var ratesFile = __dirname + '/puzzle/RATES.xml';
+var transFile = __dirname + '/puzzle/TRANS.csv';
 
 var fetchRatesData = RatesData.all(ratesFile);
 var transStream = Transactions.stream(transFile);
+var fetchTrans = Transactions.filterBySku(transStream, 'DM1182');
 
-var parse = require('csv-parse');
+Promise.all([fetchRatesData, fetchTrans])
+  .then(function(results){
+    var ratesData    = results[0]
+    var transactions = results[1]
+    var rates = Rates.all(ratesData);
 
-fetchRatesData.then(function(ratesData){
-  rates = Rates.all(ratesData);
-  console.log(rates)
-})
+    total = transactions.map(function(transaction){
+      return usdAmount(rates, transaction['amount']['currencyCode'], transaction['amount']['value']);
+    }).reduce(function(previousValue, currentValue, currentIndex, array) {
+      return previousValue + currentValue;
+    });
 
-Transactions.filterBySku(transStream, 'DM1182').then(function(trans){
-  console.log(trans);
-});
+    console.log(total.toFixed(2));
+  })
 
 
+function usdAmount(rates, currency, amount){
+  if (currency=='USD'){
+    return Number(amount);
+  }
+  else{
+    conversion = Rates.findRate(rates, currency, 'USD')['conversion'];
+    var val =  +(conversion* amount).toFixed(2);
+    return val;
+  }
+}
