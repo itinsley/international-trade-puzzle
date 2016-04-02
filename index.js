b7e5ad85fd95e29fc4898b3fb9fdf7fa2c43329d
Ian Tinsley <itinsley@gmail.com>
2016-04-02 21:13:40 +1100

    read stream

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+var Promise = require("bluebird");
+var parse = require('csv-parse');
+
 var Rates = require('./lib/rates');
 var RatesData = require('./lib/ratesData');
 var Transactions = require('./lib/transactions');
 
-var fileName = __dirname + '/puzzle/RATES.xml';
+var ratesFile = __dirname + '/puzzle/SAMPLE_RATES.xml';
+var transFile = __dirname + '/puzzle/TRANS.csv';
+var transFile = __dirname + '/puzzle/SAMPLE_TRANS.csv';
 
-RatesData.all(fileName)
-  .then(function(rates){
-    console.log("Rates including cross rates");
-    console.log(rates);
-    console.log("Transactions");
-    console.log(Transactions.all());
-  })
+var fetchRates = RatesData.all(ratesFile);
+var transStream = Transactions.stream(transFile);
+
+var parse = require('csv-parse');
+
+// // Now that setup is done, write data to the stream
+// parser.write("root:x:0:0:root:/root:/bin/bash\n");
+// parser.write("someone:x:1022:1022:a funny cat:/home/someone:/bin/bash\n");
+// Close the readable stream
+
+
+
+Transactions.findAllBySku(transStream, 'DM1182').then(function(trans){
+  console.log("end")
+  console.log(trans);
+});
+
+
