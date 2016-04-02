d5c6e9cf40571b68dde4847600178fecd3e1dd96
Ian Tinsley <itinsley@gmail.com>
2016-04-02 23:41:08 +1100

    Refactor/Tidy-up

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ var Transactions = require('./lib/transactions');
 var ratesFile = __dirname + '/puzzle/SAMPLE_RATES.xml';
 var transFile = __dirname + '/puzzle/SAMPLE_TRANS.csv';
 
-var ratesFile = __dirname + '/puzzle/RATES.xml';
-var transFile = __dirname + '/puzzle/TRANS.csv';
-
 var fetchRatesData = RatesData.all(ratesFile);
 var transStream = Transactions.stream(transFile);
 var fetchTrans = Transactions.filterBySku(transStream, 'DM1182');
@@ -21,23 +18,6 @@ Promise.all([fetchRatesData, fetchTrans])
     var transactions = results[1]
     var rates = Rates.all(ratesData);
 
-    total = transactions.map(function(transaction){
-      return usdAmount(rates, transaction['amount']['currencyCode'], transaction['amount']['value']);
-    }).reduce(function(previousValue, currentValue, currentIndex, array) {
-      return previousValue + currentValue;
-    });
-
-    console.log(total.toFixed(2));
+    console.log(Transactions.totalUsd(transactions, rates));
   })
 
-
-function usdAmount(rates, currency, amount){
-  if (currency=='USD'){
-    return Number(amount);
-  }
-  else{
-    conversion = Rates.findRate(rates, currency, 'USD')['conversion'];
-    var val =  +(conversion* amount).toFixed(2);
-    return val;
-  }
-}
