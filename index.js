7756649e402589896e09b9806b24542bbe237584
Ian Tinsley <itinsley@gmail.com>
2016-04-03 13:02:09 +1000

    Run against test data

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var Rates = require('./lib/rates');
 var RatesData = require('./lib/ratesData');
 var Transactions = require('./lib/transactions');
 
-var ratesFile = __dirname + '/puzzle/SAMPLE_RATES.xml';
-var transFile = __dirname + '/puzzle/SAMPLE_TRANS.csv';
+var ratesFile = __dirname + '/puzzle/RATES.xml';
+var transFile = __dirname + '/puzzle/TRANS.csv';
 
 var fetchRatesData = RatesData.all(ratesFile);
 var transStream = Transactions.stream(transFile);
