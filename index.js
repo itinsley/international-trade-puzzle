24aff126b7d42ca05b4e33fcb4bcb4e283982988
Ian Tinsley <itinsley@gmail.com>
2016-04-03 15:44:21 +1000

    Add factory function for Transactions
     - Improve Transactions interface with factory function that accepts
    file location as parameter

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var Promise = require("bluebird");
 var dataSources = require('./lib/dataSources')(__dirname + '/puzzle');
-var Transactions = require('./lib/transactions');
+var Transactions = require('./lib/transactions')(dataSources.transFile);
 
 dataSources.fetchRates()
   //Fetch rates first as they would be used as a dependency
   .then(function(rates){
     //Transactions are fetched as a promise as they are a filtered stream
-    fetchTrans = Transactions.fetchFilteredBySku(dataSources.transactionsStream, 'DM1182')
-    fetchTrans.then(function(transactions){
-      var result = Transactions.totalUsd(transactions, rates);
-      console.log(result);
-    })
+    Transactions.fetchFilteredBySku('DM1182')
+      .then(function(transactions){
+        var result = Transactions.totalUsd(transactions, rates);
+        console.log(result);
+      })
   });
